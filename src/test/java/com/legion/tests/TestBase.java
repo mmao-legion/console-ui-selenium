8e0eefb0a8261c9868bc8ab5dc7e296762d67a6e
Nishant Prakash <nishant.prakash@zorang.com>
2019-04-18 21:17:39 +0530

    Latest Code merged with Nishant_Zorang

--- src/test/java/com/legion/tests/TestBase.java
+++ src/test/java/com/legion/tests/TestBase.java
@@ -263,31 +263,6 @@ public abstract class TestBase {
                 setDriver(new FirefoxDriver(options));
             }
 
-//            if (getDriverType().equalsIgnoreCase("Phantom")) {
-//                System.setProperty("webdriver.chrome.driver",propertyMap.get("CHROME_DRIVER_PATH"));
-//                ChromeOptions options = new ChromeOptions();
-//                options.addArguments("disable-infobars");
-//                options.addArguments("headless");
-//                options.addArguments("window-size=1200x600");
-//                options.addArguments("test-type", "new-window", "disable-extensions","start-maximized");
-//                Map<String, Object> prefs = new HashMap<>();
-//                prefs.put("credentials_enable_service", false);
-//                prefs.put("password_manager_enabled", false);
-//                options.setExperimentalOption("prefs", prefs);
-//                options.addArguments("disable-logging", "silent", "ignore-certificate-errors");
-//                options.setExperimentalOption("useAutomationExtension", false);
-//                options.setExperimentalOption("excludeSwitches",
-//                        Collections.singletonList("enable-automation"));
-//                options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-//                options.setCapability(ChromeOptions.CAPABILITY, options);
-//                options.setCapability("chrome.switches", Arrays.asList("--disable-extensions", "--disable-logging",
-//                        "--ignore-certificate-errors", "--log-level=0", "--silent"));
-//                options.setCapability("silent", true);
-//                System.setProperty("webdriver.chrome.silentOutput", "true");
-//                setDriver(new ChromeDriver(options));
-//
-//            }
-            
             pageFactory = createPageFactory();
             LegionWebDriverEventListener webDriverEventListener = new LegionWebDriverEventListener();
             getDriver().register(webDriverEventListener);
