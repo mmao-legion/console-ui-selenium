0e62a379426245fb5055f456f3d0510e7f260542
HayaHong <hongyuanyuan@hydsoft.com>
2020-07-31 14:11:37 +0800

    add verifyComplianceViolationWidgetsAsStoreManager

--- src/test/java/com/legion/pages/LiquidDashboardPage.java
+++ src/test/java/com/legion/pages/LiquidDashboardPage.java
@@ -29,4 +29,10 @@ public interface LiquidDashboardPage {
     public HashMap <String, Float> getDataOnTodayForecast() throws Exception;
     public List<String> getDataOnSchedulesWidget() throws Exception;
     public void verifyTheContentOnTimesheetApprovalStatusWidgetLoaded(String currentWeek) throws Exception;
+    public void verifyWeekInfoOnWidget(String widgetTitle, String startdayOfWeek) throws Exception;
+    public void clickOnCarouselOnWidget(String widgetTitle, String rightOrLeft) throws Exception;
+    public void clickFirstWeekOnSchedulesGoToSchedule() throws Exception;
+    public List<String> getDataOnComplianceViolationWidget() throws Exception;
+    public void goToCompliancePage() throws Exception;
+    public List<String> getDataInCompliancePage(String location) throws Exception;
 }
