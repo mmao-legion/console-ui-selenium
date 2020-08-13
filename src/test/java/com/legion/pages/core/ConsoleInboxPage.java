f5f3419e5849888081e90633eaa83bb509628b3c
julieli1105 <liyuhe@hydsoft.com>
2020-08-13 16:08:46 +0800

    Update some methods

--- src/test/java/com/legion/pages/core/ConsoleInboxPage.java
+++ src/test/java/com/legion/pages/core/ConsoleInboxPage.java
@@ -2,9 +2,13 @@ package com.legion.pages.core;
 
 import com.legion.pages.BasePage;
 import com.legion.pages.InboxPage;
+import com.legion.utils.MyThreadLocal;
 import com.legion.utils.SimpleUtils;
+import org.apache.bcel.generic.Select;
+import org.apache.bcel.generic.Visitor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.server.handler.ClickElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import java.util.List;
@@ -18,6 +22,47 @@ public class ConsoleInboxPage  extends BasePage implements InboxPage {
     //Added by Nora
 
     //Added by Julie
+    final static String consoleInboxMenuItemText = "Inbox";
+
+    @FindBy(css = "a[ng-click=\"createAnnouncement()\"]")
+    private WebElement createAnnouncementIcon;
+
+    @FindBy(css = "option[label=\"Message\"]")
+    private WebElement messageType;
+
+    @FindBy(css = "option[label=\"Good Faith Estimate\"]")
+    private WebElement GFEType;
+
+    @FindBy(className = "console-navigation-item")
+    private List<WebElement> consoleNavigationMenuItems;
+
+    @FindBy(css = ".select-wrapper")
+    private WebElement announcementType;
+
+    @Override
+    public void clickOnInboxConsoleMenuItem() throws Exception {
+        if (consoleNavigationMenuItems.size() != 0) {
+            WebElement consoleInboxMenuElement = SimpleUtils.getSubTabElement(consoleNavigationMenuItems, consoleInboxMenuItemText);
+            clickTheElement(consoleInboxMenuElement);
+            SimpleUtils.pass("'Inbox' Console Menu loaded successfully!");
+        } else
+            SimpleUtils.fail("'Inbox' Console Menu failed to load!", false);
+    }
+
+    @Override
+    public void createGFEAnnouncement() throws Exception {
+        if (isElementLoaded(createAnnouncementIcon,10)) {
+            clickTheElement(createAnnouncementIcon);
+            if (isElementLoaded(announcementType,5)) {
+                clickTheElement(announcementType);
+                waitForSeconds(5);
+                selectByVisibleText(GFEType, "Good Faith Estimate");
+                SimpleUtils.pass("Inbox: A new announcement with type \"Good Faith Estimate\" is selected successfully");
+            } else
+                SimpleUtils.fail("Inbox: Create New Announcement window failed to load after clicking + icon",false);
+        } else
+            SimpleUtils.fail("Inbox: Create Announcement + icon failed to load",false);
+    }
 
     //Added by Marym
 
--- src/test/java/com/legion/tests/core/InboxTest.java
+++ src/test/java/com/legion/tests/core/InboxTest.java
@@ -1,8 +1,22 @@
 package com.legion.tests.core;
 
+import com.legion.pages.ControlsNewUIPage;
+import com.legion.pages.ControlsPage;
+import com.legion.pages.DashboardPage;
+import com.legion.pages.InboxPage;
 import com.legion.tests.TestBase;
+import com.legion.tests.annotations.Automated;
+import com.legion.tests.annotations.Enterprise;
+import com.legion.tests.annotations.Owner;
+import com.legion.tests.annotations.TestName;
+import com.legion.tests.data.CredentialDataProviderSource;
+import com.legion.utils.SimpleUtils;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import java.lang.reflect.Method;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class InboxTest extends TestBase {
     @Override
@@ -16,6 +30,38 @@ public class InboxTest extends TestBase {
     //Added by Nora
 
     //Added by Julie
+    @Automated(automated = "Automated")
+    @Owner(owner = "Julie")
+    @Enterprise(name = "KendraScott2_Enterprise")
+    @TestName(description = "Verify the content of operating hours and the first day of week are correct")
+    @Test(dataProvider = "legionTeamCredentialsByRoles", dataProviderClass = CredentialDataProviderSource.class)
+    public void verifyTheContentOfOperationHoursAndTheFirstDayOfWeekAsInternalAdmin(String browser, String username, String password, String location) throws Exception {
+        InboxPage inboxPage = pageFactory.createConsoleInboxPage();
+
+        // Make sure that GFE is turned on
+        ControlsNewUIPage controlsNewUIPage = pageFactory.createControlsNewUIPage();
+        ControlsPage controlsPage = pageFactory.createConsoleControlsPage();
+        controlsPage.gotoControlsPage();
+        SimpleUtils.assertOnFail("Controls Page not loaded Successfully!", controlsNewUIPage.isControlsPageLoaded(), false);
+        boolean isControlsComplianceCardSection = controlsNewUIPage.isControlsComplianceCard();
+        SimpleUtils.assertOnFail("Controls Page: Compliance Section not Loaded.", isControlsComplianceCardSection, false);
+        controlsNewUIPage.clickOnControlsComplianceSection();
+        boolean isTurnOn = true;
+        inboxPage.turnGFEToggleOnOrOff(isTurnOn);
+
+        // Get Regular hours from Controls-> Working hours -> Regular
+        String workingHoursType = "Regular";
+        controlsPage.gotoControlsPage();
+        SimpleUtils.assertOnFail("Controls page not loaded Successfully!", controlsNewUIPage.isControlsPageLoaded(), false);
+        controlsNewUIPage.clickOnControlsWorkingHoursCard();
+        SimpleUtils.assertOnFail("Working Hours Card not loaded Successfully!", controlsNewUIPage.isControlsWorkingHoursLoaded(), false);
+        controlsNewUIPage.clickOnWorkHoursTypeByText(workingHoursType);
+        LinkedHashMap<String, List<String>> regularHours = controlsNewUIPage.getRegularWorkingHours();
+
+        // Create a GFE announcement to verify its content of operation hours and the first day of week
+        inboxPage.clickOnInboxConsoleMenuItem();
+        inboxPage.createGFEAnnouncement();
+    }
 
     //Added by Marym
 
