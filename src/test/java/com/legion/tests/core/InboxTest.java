52b3d9dfc31c6c86cb3e73391b60ad003c698c4c
MaryMao619 <67356336+MaryMao619@users.noreply.github.com>
2020-08-13 16:54:07 +0800

    Revert "add test case:verifyTurnOffGFEAsInternalAdmin"
    This reverts commit 93014130ff3ca547e38a2659482e4280a6904e5c.

--- src/test/java/com/legion/tests/core/InboxTest.java
+++ src/test/java/com/legion/tests/core/InboxTest.java
@@ -1,20 +1,8 @@
 package com.legion.tests.core;
 
-import com.legion.pages.*;
 import com.legion.tests.TestBase;
-import com.legion.tests.annotations.Automated;
-import com.legion.tests.annotations.Enterprise;
-import com.legion.tests.annotations.Owner;
-import com.legion.tests.annotations.TestName;
-import com.legion.tests.data.CredentialDataProviderSource;
-import com.legion.utils.SimpleUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import java.lang.reflect.Method;
-import java.util.List;
 
 public class InboxTest extends TestBase {
     @Override
@@ -30,29 +18,6 @@ public class InboxTest extends TestBase {
     //Added by Julie
 
     //Added by Marym
-    @Automated(automated = "Automated")
-    @Owner(owner = "Mary")
-    @Enterprise(name = "KendraScott2_Enterprise")
-    @TestName(description = "Verify turn off GFE ")
-    @Test(dataProvider = "legionTeamCredentialsByRoles", dataProviderClass = CredentialDataProviderSource.class)
-    public void verifyTurnOffGFEAsInternalAdmin(String browser, String username, String password, String location) throws Exception {
-        DashboardPage dashboardPage = pageFactory.createConsoleDashboardPage();
-        SimpleUtils.assertOnFail("Dashboard page not loaded successfully!", dashboardPage.isDashboardPageLoaded(), false);
-
-        ControlsNewUIPage controlsNewUIPage = pageFactory.createControlsNewUIPage();
-        controlsNewUIPage.clickOnControlsConsoleMenu();
-        SimpleUtils.assertOnFail("Controls page not loaded successfully!", controlsNewUIPage.isControlsPageLoaded(), false);
-
-        controlsNewUIPage.clickOnControlsComplianceSection();
-        SimpleUtils.assertOnFail("Compliance page not loaded successfully!", controlsNewUIPage.isCompliancePageLoaded(), false);
-
-        InboxPage inboxPage = pageFactory.createConsoleInboxPage();
-        inboxPage.turnGFEToggleOnOrOff(false);
-
-
-
-
-    }
 
     //Added by Haya
 }
