23a3ef862f65fe41bfda552a320b62ab9e75c958
Estelle121 <estelle@legion.co>
2021-02-19 14:58:04 +0800

    fix failed scripts

--- src/test/java/com/legion/pages/core/OpsPortalLocationsPage.java
+++ src/test/java/com/legion/pages/core/OpsPortalLocationsPage.java
@@ -316,7 +316,7 @@ public class OpsPortalLocationsPage extends BasePage implements LocationsPage {
 		if (isElementEnabled(selectALocationTitle,5)) {
 			searchInputInSelectALocation.sendKeys(searchCharactor);
 			searchInputInSelectALocation.sendKeys(Keys.ENTER);
-			waitForSeconds(5);
+			waitForSeconds(15);
 			if (locationRowsInSelectLocation.size()>0) {
 				WebElement firstRow = locationRowsInSelectLocation.get(index).findElement(By.cssSelector("input[type=\"checkbox\"]"));
 				click(firstRow);
