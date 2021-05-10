2532f957e283fe129262281fba91acf345342169
Estelle L Yan <60596040+Estelle121@users.noreply.github.com>
2021-05-10 22:00:38 +0800

    update browser capability

--- src/test/java/com/legion/tests/TestBase.java
+++ src/test/java/com/legion/tests/TestBase.java
@@ -256,7 +256,7 @@ public abstract class TestBase {
     private void createRemoteChrome(String url){
         MyThreadLocal myThreadLocal = new MyThreadLocal();
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("browserName", "Edge");
+        caps.setCapability("browserName", "chrome");
 //        caps.setCapability("version", "5.4.0-1029-aws");
         caps.setCapability("platform", "LINUX");
         caps.setCapability("idleTimeout", 150);
