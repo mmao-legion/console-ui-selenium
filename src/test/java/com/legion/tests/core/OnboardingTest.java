8339748870f9d44ce289d1a88d2ac4f69cb1867a
NoraLi-0114 <59428870+NoraLi-0114@users.noreply.github.com>
2021-05-20 15:08:05 +0800

    Add the test data for onboarding

--- src/test/java/com/legion/tests/core/OnboardingTest.java
+++ src/test/java/com/legion/tests/core/OnboardingTest.java
@@ -8,42 +8,56 @@ import com.legion.tests.annotations.Enterprise;
 import com.legion.tests.annotations.Owner;
 import com.legion.tests.annotations.TestName;
 import com.legion.tests.data.CredentialDataProviderSource;
+import com.legion.utils.JsonUtil;
+import com.legion.utils.MyThreadLocal;
 import com.legion.utils.SimpleUtils;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 import static com.legion.utils.MyThreadLocal.getDriver;
 
 public class OnboardingTest  extends TestBase {
+
+    private static HashMap<String, String> testDataMap = JsonUtil.getPropertiesFromJsonFile("src/test/resources/OnboardingTestData.json");
+    private String nonSSOEnterprise = propertyMap.get("KendraScott2_Enterprise");
+    private String ssoEnterprise = propertyMap.get("Dgch_Enterprise");
+    private String currentLocation = "";
+
     @Override
     @BeforeMethod()
     public void firstTest(Method testMethod, Object[] params) throws Exception{
         try {
             this.createDriver((String) params[0], "83", "Window");
             visitPage(testMethod);
-            loginToLegionAndVerifyIsLoginDone((String) params[1], (String) params[2], (String) params[3]);
+            loginToLegionAndVerifyIsLoginDoneWithoutUpdateUpperfield((String) params[1], (String) params[2], (String) params[3]);
+            LocationSelectorPage locationSelectorPage = pageFactory.createLocationSelectorPage();
+            LoginPage loginPage = pageFactory.createConsoleLoginPage();
+            if (MyThreadLocal.getEnterprise().equalsIgnoreCase(nonSSOEnterprise)) {
+                currentLocation = testDataMap.get("Non-SSO_Location");
+                locationSelectorPage.changeUpperFields(testDataMap.get("Non-SSO_Upperfields"));
+                locationSelectorPage.changeLocation(currentLocation);
+            } else if (MyThreadLocal.getEnterprise().equalsIgnoreCase(ssoEnterprise)) {
+                currentLocation = testDataMap.get("SSO_Location");
+                locationSelectorPage.changeUpperFields(testDataMap.get("SSO_Upperfields"));
+                locationSelectorPage.changeLocation(currentLocation);
+            }
+            boolean isLoginDone = loginPage.isLoginDone();
+            loginPage.verifyLoginDone(isLoginDone, currentLocation);
         } catch (Exception e){
             SimpleUtils.fail(e.getMessage(), false);
         }
     }
 
-
     @Automated(automated ="Automated")
     @Owner(owner = "Mary")
     @Enterprise(name = "KendraScott2_Enterprise")
-    @TestName(description = "Verify the onboarding flow when the rehired user has an account and status change to Active")
+    @TestName(description = "Verify the onboarding flow for New hire and status changed to Active (Non-SSO)")
     @Test(dataProvider = "legionTeamCredentialsByRoles", dataProviderClass= CredentialDataProviderSource.class)
-    public void verifyTheOnboardingFlowWhenTheRehiredUserHasAnAccountAndStatusChangeToActiveAsStoreManager(String browser, String username, String password, String location) {
+    public void verifyTheOnboardingFlowForNewHireAndStatusChangeToActiveAsInternalAdmin(String browser, String username, String password, String location) {
         try {
-            DashboardPage dashboardPage = pageFactory.createConsoleDashboardPage();
-            SimpleUtils.assertOnFail("DashBoard Page not loaded Successfully!",dashboardPage.isDashboardPageLoaded() , false);
-
-            LocationSelectorPage locationSelectorPage = pageFactory.createLocationSelectorPage();
-            locationSelectorPage.changeDistrict("Demo District");
-            locationSelectorPage.changeLocation("Test_For_Onboarding");
-
             verifyOnboardingFlow("Yes");
 
         } catch (Exception e){
