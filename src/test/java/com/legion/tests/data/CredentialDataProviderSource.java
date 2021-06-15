16d05f15becfa20dde6fc63da351ed2e1954343b
HayaHong <hongyuanyuan@hydsoft.com>
2021-06-15 11:04:59 +0800

    update provider file and add vailqacn credentials file

--- src/test/java/com/legion/tests/data/CredentialDataProviderSource.java
+++ src/test/java/com/legion/tests/data/CredentialDataProviderSource.java
@@ -1,6 +1,7 @@
 package com.legion.tests.data;
 
 import com.legion.utils.JsonUtil;
+import com.legion.utils.MyThreadLocal;
 import com.legion.utils.SimpleUtils;
 
 import java.lang.reflect.Array;
@@ -48,10 +49,11 @@ public class CredentialDataProviderSource {
          }
          HashMap<String, Object[][]> userCredentials = SimpleUtils
         		 .getEnvironmentBasedUserCredentialsFromJson(fileName);
-         
         	 for(Map.Entry<String, Object[][]> entry : userCredentials.entrySet())
              {
-                 if(testMethod.getName().contains(entry.getKey()))
+                 String testFullName = testMethod.getName();
+                 testFullName = testFullName + "Of" + testMethod.getDeclaringClass().getName().split(".")[testMethod.getDeclaringClass().getName().split(".").length-1];
+                 if(testFullName.contains(entry.getKey()))
                  {
                      return SimpleUtils.concatenateObjects(browserDataProvider(testMethod), entry.getValue()) ;
                  }
