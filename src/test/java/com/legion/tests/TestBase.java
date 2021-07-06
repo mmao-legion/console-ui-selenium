315604cc0fc276b419b98c082dad13e2c446b0b3
HayaHong <hongyuanyuan@hydsoft.com>
2021-07-06 19:08:37 +0800

    reduce requests sent to test rail

--- src/test/java/com/legion/tests/TestBase.java
+++ src/test/java/com/legion/tests/TestBase.java
@@ -54,6 +54,7 @@ import static com.jayway.restassured.RestAssured.baseURI;
 import static com.jayway.restassured.RestAssured.given;
 import static com.legion.utils.MyThreadLocal.*;
 import static com.legion.utils.MyThreadLocal.getDriver;
+import static com.legion.utils.SimpleUtils.addResultForTest;
 
 //import org.apache.log4j.Logger;
 
@@ -291,6 +292,7 @@ public abstract class TestBase {
 
     @AfterMethod(alwaysRun = true)
     protected void tearDown(Method method,ITestResult result) throws IOException {
+        addResultForTest();
         ExtentTestManager.getTest().info("tearDown started");
         if (Boolean.parseBoolean(propertyMap.get("close_browser"))) {
             try {
