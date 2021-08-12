707010e7db7031042e5c0ca62a7443e84e598785
MaryMao619 <67356336+MaryMao619@users.noreply.github.com>
2021-08-12 14:30:15 +0800

    fix failed case

--- src/test/java/com/legion/tests/core/ScheduleCopyImprovementTest.java
+++ src/test/java/com/legion/tests/core/ScheduleCopyImprovementTest.java
@@ -72,7 +72,7 @@ public class ScheduleCopyImprovementTest extends TestBase {
 
     @Automated(automated = "Automated")
     @Owner(owner = "Mary")
-    @Enterprise(name = "CinemarkWkdy_Enterprise")
+    @Enterprise(name = "KendraScott2_Enterprise")
     @TestName(description = "Validate the unassigned shifts convert to open shifts setting in OP")
     @Test(dataProvider = "legionTeamCredentialsByRoles", dataProviderClass = CredentialDataProviderSource.class)
     public void validateTheUnassignedShiftsConvertToOpenShiftsSettingInOPAsInternalAdmin(String browser, String username, String password, String location) throws Exception {
