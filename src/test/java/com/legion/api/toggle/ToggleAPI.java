a92c299c360f2024416ebdaaf87e3fa72cb2a44c
MaryMao619 <67356336+MaryMao619@users.noreply.github.com>
2021-11-08 14:56:14 +0800

    Update ToggleAPI.java

--- src/test/java/com/legion/api/toggle/ToggleAPI.java
+++ src/test/java/com/legion/api/toggle/ToggleAPI.java
@@ -3,6 +3,7 @@ package com.legion.api.toggle;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
 import com.legion.api.common.EnterpriseId;
+import com.legion.utils.SimpleUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,71 +14,45 @@ import static com.jayway.restassured.RestAssured.given;
 public class ToggleAPI {
 
     public static void enableToggle(String toggleName, String username, String password) {
+        try {
 
-        Response response = given().params("enterpriseName",System.getProperty("enterprise"),"sourceSystem","legion","passwordPlainText",password,"userName",username)
-                .when().get("https://rc-enterprise.dev.legion.work/legion/authentication/login").then().statusCode(200).extract().response();
-        String sessionId = response.header("sessionid");
-        //get toggle to confirm the switch is on or off
-        Response response2= given().log().all().header("sessionId",sessionId).param("toggle", toggleName).when().get("https://rc-enterprise.dev.legion.work/legion/toggles").then().log().all().extract().response();
-        String enabled = response2.jsonPath().get("record.enabled").toString();
+            Response response = given().params("enterpriseName",System.getProperty("enterprise"),"sourceSystem","legion","passwordPlainText",password,"userName",username)
+                    .when().get("https://rc-enterprise.dev.legion.work/legion/authentication/login").then().statusCode(200).extract().response();
+            String sessionId = response.header("sessionid");
+            //get toggle to confirm the switch is on or off
+            Response response2= given().log().all().header("sessionId",sessionId).param("toggle", toggleName).when().get("https://rc-enterprise.dev.legion.work/legion/toggles").then().log().all().extract().response();
+            String enabled = response2.jsonPath().get("record.enabled").toString();
 
-        List<HashMap> rules = new ArrayList<>();
-        HashMap<String, Object> rulesValue = new HashMap<>();
-        rulesValue.put("enterpriseName", EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
-        List<String> businesses = new ArrayList<>();
-        businesses.add(EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
-        rulesValue.put("businessName", businesses);
-        rules.add(rulesValue);
+            List<HashMap> rules = new ArrayList<>();
+            HashMap<String, Object> rulesValue = new HashMap<>();
+            rulesValue.put("enterpriseName", EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
+            List<String> businesses = new ArrayList<>();
+            businesses.add(EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
+            rulesValue.put("businessName", businesses);
+            rules.add(rulesValue);
 
-        if (enabled.equals("false")) {
-            HashMap<String, Object> recordContext = new HashMap<>();
-            recordContext.put( "name", toggleName);
-            recordContext.put("controlValue", "");
-            recordContext.put("defaultValue", true);
-            recordContext.put("rules", rules);
-            HashMap<String, Object>  jsonAsMap = new HashMap<>();
-            jsonAsMap.put("level", "Enterprise");
-            jsonAsMap.put("valid", true);
-            jsonAsMap.put("record",recordContext);
+            if (enabled.equals("false")) {
+                HashMap<String, Object> recordContext = new HashMap<>();
+                recordContext.put( "name", toggleName);
+                recordContext.put("controlValue", "");
+                recordContext.put("defaultValue", true);
+                recordContext.put("rules", rules);
+                HashMap<String, Object>  jsonAsMap = new HashMap<>();
+                jsonAsMap.put("level", "Enterprise");
+                jsonAsMap.put("valid", true);
+                jsonAsMap.put("record",recordContext);
 
-            Response responseAfterDisable= given().log().all().headers("sessionId",sessionId).contentType(ContentType.JSON).body(jsonAsMap)
-                    .when().post("https://rc-enterprise.dev.legion.work/legion/toggles").then().log().all().extract().response();
-            responseAfterDisable.then().statusCode(200);
+                Response responseAfterDisable= given().log().all().headers("sessionId",sessionId).contentType(ContentType.JSON).body(jsonAsMap)
+                        .when().post("https://rc-enterprise.dev.legion.work/legion/toggles").then().log().all().extract().response();
+                responseAfterDisable.then().statusCode(200);
 
+            }
+        } catch (Exception e) {
+            SimpleUtils.report("Failed to enable toggle: " + toggleName);
         }
     }
 
     public static void disableToggle(String toggleName, String username, String password) {
-
-        Response response = given().params("enterpriseName",System.getProperty("enterprise"),"sourceSystem","legion","passwordPlainText",password,"userName",username)
-                .when().get("https://rc-enterprise.dev.legion.work/legion/authentication/login").then().statusCode(200).extract().response();
-        String sessionId = response.header("sessionid");
-        //get toggle to confirm the switch is on or off
-        Response response2= given().log().all().header("sessionId",sessionId).param("toggle", toggleName).when().get("https://rc-enterprise.dev.legion.work/legion/toggles").then().log().all().extract().response();
-        String enabled = response2.jsonPath().get("record.enabled").toString();
-
-        List<HashMap> rules = new ArrayList<>();
-        HashMap<String, Object> rulesValue = new HashMap<>();
-        rulesValue.put("enterpriseName", EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
-        List<String> businesses = new ArrayList<>();
-        businesses.add(EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
-        rulesValue.put("businessName", businesses);
-        rules.add(rulesValue);
-
-        if (enabled.equals("true")) {
-            HashMap<String, Object> recordContext = new HashMap<>();
-            recordContext.put( "name", toggleName);
-            recordContext.put("controlValue", "");
-            recordContext.put("defaultValue", false);
-            recordContext.put("rules", rules);
-            HashMap<String, Object>  jsonAsMap = new HashMap<>();
-            jsonAsMap.put("level", "Enterprise");
-            jsonAsMap.put("valid", true);
-            jsonAsMap.put("record",recordContext);
-
-            Response responseAfterDisable= given().log().all().headers("sessionId",sessionId).contentType(ContentType.JSON).body(jsonAsMap)
-                    .when().post("https://rc-enterprise.dev.legion.work/legion/toggles").then().log().all().extract().response();
-            responseAfterDisable.then().statusCode(200);
         try {
 
             Response response = given().params("enterpriseName", System.getProperty("enterprise"), "sourceSystem", "legion", "passwordPlainText", password, "userName", username)
@@ -89,9 +64,9 @@ public class ToggleAPI {
 
             List<HashMap> rules = new ArrayList<>();
             HashMap<String, Object> rulesValue = new HashMap<>();
-            rulesValue.put("enterpriseName", EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase()).getValue());
+            rulesValue.put("enterpriseName", EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
             List<String> businesses = new ArrayList<>();
-            businesses.add(EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase()).getValue());
+            businesses.add(EnterpriseId.valueOf(System.getProperty("enterprise").toLowerCase().replace("-", "")).getValue());
             rulesValue.put("businessName", businesses);
             rules.add(rulesValue);
 
