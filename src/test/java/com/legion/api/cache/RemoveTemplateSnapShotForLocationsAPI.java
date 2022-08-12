25d98b238e5058f225f12bb4378dd4dd170f5636
MaryMao619 <67356336+MaryMao619@users.noreply.github.com>
2022-08-12 20:59:57 +0800

    Update remove snap shot API

--- src/test/java/com/legion/api/cache/RemoveTemplateSnapShotForLocationsAPI.java
+++ src/test/java/com/legion/api/cache/RemoveTemplateSnapShotForLocationsAPI.java
@@ -5,7 +5,9 @@ import com.legion.tests.TestBase;
 import com.legion.utils.JsonUtil;
 import com.legion.utils.SimpleUtils;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -16,28 +18,37 @@ public class RemoveTemplateSnapShotForLocationsAPI {
     public static void removeTemplateSnapShotForLocationsAPI(String username, String password) {
         try {
             HashMap<String, String> locationBusinessID = JsonUtil.getPropertiesFromJsonFile("src/test/resources/LocationBusinessID.json");
-            int dayOfYearCurrentDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
-            int dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-            int lastSundayOfYear = dayOfYearCurrentDay - (dayOfWeek-1)-7;
-            String currentYear = TestBase.getCurrentTime().substring(0, 4);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar cal = Calendar.getInstance();
+//            Date date = sdf.parse("2001-01-10");
+            Date date = new Date();
+            cal.setTime(date);
+
+            int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+            cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - dayOfWeek -14);
+            date = cal.getTime();
+
             Response response = given().params("enterpriseName",System.getProperty("enterprise"),"sourceSystem","legion","passwordPlainText",password,"userName",username)
                     .when().get("https://rc-enterprise.dev.legion.work/legion/authentication/login").then().statusCode(200).extract().response();
             String sessionId = response.header("sessionid");
             Set<String> keySet = locationBusinessID.keySet();
             for (String key: keySet) {
+                cal.setTime(date);
                 String businessId = locationBusinessID.get(key);
                 for (int i=0; i<4; i++) {
+                    cal.add(Calendar.DATE, 7);
+                    int weekStartDayOfTheYear = cal.get(Calendar.DAY_OF_YEAR);
+                    int currentYear = Integer.parseInt(sdf.format(cal.getTime()).substring(0, 4));
                     Response cacheResponse = given().log().all()
                             .header("sessionId", sessionId)
                             .param("year", currentYear)
-                            .param("weekStartDayOfTheYear",lastSundayOfYear+ i*7)
+                            .param("weekStartDayOfTheYear",weekStartDayOfTheYear)
                             .param("businessId",businessId)
                             .when().get("https://rc-enterprise.dev.legion.work/legion/configTemplate/removeTemplateSnapShotForLocations")
                             .then().log().all().extract().response();
                     cacheResponse.then().statusCode(200);
                 }
             }
-
         } catch (Exception e) {
             SimpleUtils.report("Failed to refresh the cache!");
         }
