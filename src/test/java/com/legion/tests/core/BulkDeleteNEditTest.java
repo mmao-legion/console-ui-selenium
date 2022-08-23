531db6b90fa9cb7ccaa223021128c4a6ab8d85c6
NoraLi-0114 <59428870+NoraLi-0114@users.noreply.github.com>
2022-08-23 10:30:28 +0800

    Fix the failed scripts

--- src/test/java/com/legion/tests/core/BulkDeleteNEditTest.java
+++ src/test/java/com/legion/tests/core/BulkDeleteNEditTest.java
@@ -420,7 +420,7 @@ public class BulkDeleteNEditTest extends TestBase {
             editShiftPage.verifyTheFunctionalityOfOffsetTime(null, "1", null, true);
             // Verify the functionality of Hours input in Start Time section
             editShiftPage.verifyTheFunctionalityOfOffsetTime("11", null, null, true);
-            editShiftPage.verifyTheFunctionalityOfOffsetTime("12", null, null, true);
+            editShiftPage.verifyTheFunctionalityOfOffsetTime("13", null, null, true);
             // Verify the functionality of Early/Late select in Start Time section
             editShiftPage.verifyTheFunctionalityOfOffsetTime(null, null, "Early", true);
             editShiftPage.verifyTheFunctionalityOfOffsetTime(null, null, "Late", true);
@@ -435,7 +435,7 @@ public class BulkDeleteNEditTest extends TestBase {
             editShiftPage.verifyTheFunctionalityOfOffsetTime(null, "1", null, false);
             // Verify the functionality of Hours input in End Time section
             editShiftPage.verifyTheFunctionalityOfOffsetTime("11", null, null, false);
-            editShiftPage.verifyTheFunctionalityOfOffsetTime("12", null, null, false);
+            editShiftPage.verifyTheFunctionalityOfOffsetTime("13", null, null, false);
             // Verify the functionality of Early/Late select in End Time section
             editShiftPage.verifyTheFunctionalityOfOffsetTime(null, null, "Early", false);
             editShiftPage.verifyTheFunctionalityOfOffsetTime(null, null, "Late", false);
