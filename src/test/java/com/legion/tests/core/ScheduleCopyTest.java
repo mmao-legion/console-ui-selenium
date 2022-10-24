a461a725d09274068a54dc462bc3ec5f408a700d
MaryMao619 <67356336+MaryMao619@users.noreply.github.com>
2022-10-24 16:21:25 +0800

    Fix failed auto scripts

--- src/test/java/com/legion/tests/core/ScheduleCopyTest.java
+++ src/test/java/com/legion/tests/core/ScheduleCopyTest.java
@@ -244,7 +244,7 @@ public class ScheduleCopyTest extends TestBase {
                 newShiftPage.moveSliderAtCertainPoint("8am", ScheduleTestKendraScott2.shiftSliderDroppable.StartPoint.getValue());
                 newShiftPage.clickRadioBtnStaffingOption(ScheduleTestKendraScott2.staffingOption.AssignTeamMemberShift.getValue());
                 newShiftPage.clickOnCreateOrNextBtn();
-                newShiftPage.searchTeamMemberByName(location);
+                newShiftPage.searchTeamMemberByName(location.substring(0, 7));
                 newShiftPage.clickOnOfferOrAssignBtn();
                 scheduleMainPage.saveSchedule();
             }
